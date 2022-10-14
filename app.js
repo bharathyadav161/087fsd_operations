28acb646827756254626d3dfd0da120f1f16a633
Bharath Kumar Polaboina <97655326+bharathyadav161@users.noreply.github.com>
2022-10-14 11:55:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var dishRouter  = require('./routes/dishRouter')
+var promRouter  = require('./routes/promoRouter')
+
+const mongoose = require('mongoose')
+const Dishes = require('./models/dishes')
+
+const url = 'mongodb://localhost:27017/conFusion'
+
+const connect = mongoose.connect(url)
+connect
+.then((db)=>{
+  console.log("Connected properly server")
+} , (err)=>{
+  console.log("Error :" +err)
+})
+
+
+var app = express();
+
+// view engine setup
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+function auth(req , res , next){
+    console.log(req.headers);
+
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+      var err= new Error("You are not authenticated!!")
+
+      res.setHeader('WWW_Authenticate' , 'Basic')
+      err.status = 401;
+      next(err)
+    }
+
+    var  auth = new Buffer(authHeader.split(' ')[1] , 'base64').toString().split(':');
+
+    var username = auth[0];
+    var password = auth[1];
+
+    if(username==='admin' && password==='password'){
+      next();
+    }
+    else{
+      var err= new Error("You are not authenticated!!")
+
+      res.setHeader('WWW_Authenticate' , 'Basic')
+      err.status = 401;
+      next(err)
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/dishes',dishRouter );
+app.use('/promotions', promRouter);
+
+app.set('views', path.join(__dirname, 'views'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
